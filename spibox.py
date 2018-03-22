55c5cc5cea9c0990e8b095ee58a0e977a225fc4e
JoseaScripts <32377523+JoseaScripts@users.noreply.github.com>
2018-03-22 19:41:12 +0100

    Update spibox.py
    Rutas modificadas.
    Pendiente testeo.

--- spibox.py
+++ spibox.py
@@ -19,8 +19,8 @@ LUZ = 26
 def photo():
     for i in range(1,2):
         capturename = get_file_name()
-        print('Motion detected! Taking snapshot')
-        cmd="raspistill -w 640 -h 480 -n -t 10 -q 10 -e jpg -th none -o /home/pi/spibox/capture/" + capturename+"_%d.jpg" % (i)
+        print('Movimiento detectado! Tomando fotos.')
+        cmd="raspistill -w 640 -h 480 -n -t 10 -q 10 -e jpg -th none -o /home/pi/SPiBox/capturas/" + capturename+"_%d.jpg" % (i)
         camerapid = subprocess.call(cmd,shell=True)
 
 GPIO.setmode(GPIO.BCM)
@@ -28,22 +28,22 @@ GPIO.setup(PIR, GPIO.IN, GPIO.PUD_DOWN)
 #GPIO.setup(LUZ, GPIO.OUT)
 GPIO.setup(LUZ, GPIO.OUT, initial=1)    # set initial value option (1 or 0)  
 try:
-    print "Turning on motion sensor..."
+    print "Activando sensor de movimiento..."
  
     # Loop until PIR indicates nothing is happening
     while GPIO.input(PIR)==1:
         Current_State  = 0
  
-    print "  Sensor ready"
+    print "  Sensor listo."
  
     while True:
-        print('Waiting for movement')
+        print('Esperando actividad...')
         GPIO.wait_for_edge(PIR,GPIO.RISING)
         photo()
 	# Enciendo la luz -> puedo llamar a una fucncion
 	GPIO.output(LUZ, GPIO.LOW)		# De momento no funciona
 
 except KeyboardInterrupt:
-  print "  Bye for now"
+  print "  Programa desactivado."
   # Reset GPIO
   GPIO.cleanup()
